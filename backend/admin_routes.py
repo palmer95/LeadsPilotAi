80b5c4a81d3c978c71edd0507ee391eb35caa8d4
Jt Palmer <palmerj@Jts-MBP-2.lan>
2025-05-16 12:32:39 -0700

    fix: faqs route

--- backend/admin_routes.py
+++ backend/admin_routes.py
@@ -1,6 +1,6 @@
 # backend/admin_routes.py
 from flask import Blueprint, request, jsonify, session
-from db import SessionLocal, Lead
+from db import SessionLocal, Lead, FAQ
 from flask_cors import CORS
 
 bp = Blueprint('admin_routes', __name__, url_prefix='/api/admin')
@@ -10,4 +10,11 @@ def get_leads():
     db = SessionLocal()
     leads = db.query(Lead).all()
     result = [{"id": lead.id, "name": lead.name, "email/phone": lead.email} for lead in leads]
+    return jsonify(result), 200
+
+@bp.route('/faqs', methods=['GET'])
+def get_faqs():
+    db = SessionLocal()
+    faqs = db.query(FAQ).all()
+    result = [{"id": faq.id, "question": faq.question, "answer": faq.answer} for faq in faqs]
     return jsonify(result), 200
